56da00f14443b95d61e323ea76d4e80626b28417
Jou <66706240+JouJou411@users.noreply.github.com>
2022-02-23 09:07:17 -0600

    Ejercicio en clase

--- Recursion/src/domain/Recursion.java
+++ Recursion/src/domain/Recursion.java
@@ -9,20 +9,26 @@ package domain;
  *
  * @author Joabp
  */
-public class Recursion {
+public class Recursion
+{
 
-    public void recursion1(int[] arreglo, int i) {
-        if (i >= 0) {
+    public void recursion1(int[] arreglo, int i)
+    {
+        if (i >= 0)
+        {
             recursion2(arreglo, arreglo.length - 1);
             recursion1(arreglo, i - 1);
         }
     }
 
-    public void recursion2(int[] arreglo, int i) {
-        if (i > 0) {
+    public void recursion2(int[] arreglo, int i)
+    {
+        if (i > 0)
+        {
             int elementoActual = arreglo[i - 1],
                     elementoSiguiente = arreglo[i];
-            if (elementoActual > elementoSiguiente) {
+            if (elementoActual > elementoSiguiente)
+            {
                 arreglo[i - 1] = elementoSiguiente;
                 arreglo[i] = elementoActual;
             }
@@ -30,10 +36,53 @@ public class Recursion {
         }
     }
 
-    public void mostrar(int[] arreglo, int i) {
-        if (i >= 0) {
+    public void mostrar(int[] arreglo, int i)
+    {
+        if (i >= 0)
+        {
             mostrar(arreglo, i - 1);
             System.out.println(arreglo[i]);
+        }
+    }
+
+    public int factorial(int n)
+    {
+        int f = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            f *= i;
+        }
+        return f;
+    }
+
+    public int factorialRecur(int n, int f, int i)
+    {
+        if (i <= n)
+        {
+            f = i * factorialRecur(n, f, i + 1);
+        }
+        return f;
+    }
+
+    public double promedio(int[] arr)
+    {
+        double prom = 0;
+        for (int i = 0; i < arr.length; i++)
+        {
+            prom += arr[i];
+        }
+        double resultado = prom / arr.length;
+        return resultado;
+    }
+
+    public double promedioRecur(int[] arr, double prom, int i)
+    {
+        if (i >= 0)
+        {
+            prom += arr[i];
+            return promedioRecur(arr, prom, i - 1);
+        }  else {
+            return prom += arr[i];
         }
     }
 }
--- Recursion/src/test/TestRecursion.java
+++ Recursion/src/test/TestRecursion.java
@@ -11,12 +11,22 @@ import domain.Recursion;
  *
  * @author Joabp
  */
-public class TestRecursion {
+public class TestRecursion
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Recursion r = new Recursion();
-        int arr[] = {5, 8, 4, 9, 2, 5};
-        r.recursion1(arr, arr.length - 1);
-        r.mostrar(arr, arr.length - 1);
+        int arr[] =
+        {
+            5, 8, 4, 9, 2, 5
+        };
+//        r.recursion1(arr, arr.length - 1);
+//        r.mostrar(arr, arr.length - 1);
+//        int x = r.factorial(6);
+//        System.out.println("x = " + x);
+//        System.out.println("y = " + r.factorialRecur(6, 1, 2));
+        System.out.println("Promedio: " + r.promedio(arr));
+        System.out.println("Promedio: " + r.promedioRecur(arr, 0, arr.length - 1));
     }
 }
